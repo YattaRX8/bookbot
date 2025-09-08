6fd1e518130d2288d231efea32a1a56cc0360c43
YattaRX8 <LJNassraway@gmail.com>
2025-09-07 19:23:33 -0700

    built bookbot according to lesson plan

---
+++ main.py
@@ -0,0 +1,40 @@
+import sys
+from stats import (
+    get_word_count,
+    get_char_dict,
+    sort_dict,
+)
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def print_report(book_path, num_words, sorted_dict):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+
+    for dict in sorted_dict:
+        if dict["name"].isalpha():
+            print(f"{dict["name"]}: {dict["num"]}")
+    
+    print("============= END ===============")
+
+def main():
+    
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_path = sys.argv[1]
+    book_text = get_book_text(book_path)
+    num_words = get_word_count(book_text)
+    chars_dict = get_char_dict(book_text)
+    sorted_dict = sort_dict(chars_dict)
+
+
+    print_report(book_path, num_words, sorted_dict)
+    
+main()
---
+++ stats.py
@@ -0,0 +1,34 @@
+def get_word_count(text):
+    split_text = text.split(None)
+    count = 0
+    for words in split_text:
+        count += 1
+    return count
+
+def get_char_dict(text):
+    text = text.lower()
+    char_dict= {}
+    
+    for char in text:
+        if char in char_dict:
+            char_dict[char] += 1
+        else:
+            char_dict[char] = 1
+            
+    return char_dict
+
+def sort_on(items):
+    return items["num"]
+
+def sort_dict(dict):
+    char_dict_list = []
+
+    for d in dict:
+        new_dict = {"name": d, "num": dict[d]}
+        char_dict_list.append(new_dict)
+
+    char_dict_list.sort(reverse=True, key=sort_on)
+
+    return char_dict_list
+
+
